3f1a7167059dea26693df459764a756045988230
Aditya Raj <32588231+aditya201551@users.noreply.github.com>
2023-10-03 23:27:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (94.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -8,13 +8,6 @@ class Solution {
             nums[1]=0;
         }
 
-        int zeroCount=0;
-        for(int i: nums){
-            if(i==0){
-                zeroCount++;
-            }
-        }
-
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
