2b5b94d2cb7fcd61e2f871777d0208a53102815f
Aditya Raj <aditya201551@gmail.com>
2024-05-29 08:15:34 +0530

    Time: 14 ms (5.55%) | Memory: 43.2 MB (23.48%) - LeetSync

--- 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.java
+++ 3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.java
@@ -9,26 +9,31 @@ class Solution {
 
         String arr[] = time.split(":");
 
-        String hr = arr[0], min = arr[1];
-        if (hr.charAt(0) == '?') {
-            if (hr.charAt(1) >= '2' && hr.charAt(1) <= '9')
-                hr = "0" + hr.charAt(1);
+        // String hr = "", min = "";
+        char h1 = arr[0].charAt(0), h2 = arr[0].charAt(1), m1 = arr[1].charAt(0), m2 = arr[1].charAt(1);
+
+        System.out.println(h1 + " " + h2 + " " + m1 + " " + m2);
+        if (h1 == '?') {
+            if (h2 >= '2' && h2 <= '9')
+                h1 = '0';
             else
-                hr = "1" + hr.charAt(1);
-        }
-        if (hr.charAt(1) == '?') {
-            if (hr.charAt(0) == '0')
-                hr = "09";
-            else
-                hr = "11";
+                h1 = '1';
         }
 
-        if (min.charAt(0) == '?')
-            min = "5" + min.charAt(1);
-        if (min.charAt(1) == '?')
-            min = min.charAt(0) + "9";
+        if (h2 == '?') {
+            if (h1 == '1')
+                h2 = '1';
+            else
+                h2 = '9';
+        }
 
-        return hr + ":" + min;
+        if (m1 == '?')
+            m1 = '5';
+
+        if (m2 == '?')
+            m2 = '9';
+
+        return ""+h1+h2+":"+m1+m2;
 
     }
 }
